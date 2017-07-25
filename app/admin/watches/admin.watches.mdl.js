09e4b6cb237106e17d1842dda76f6f0ceb56d05e
James Le <le_j6@denison.edu>
2017-07-25 15:07:32 -0700

    Create Admin Watches Module

---
+++ app/admin/watches/admin.watches.mdl.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('admin.watches', [
+            'admin.watches.edit',
+            'admin.watches.add'
+        ])
+        .config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('admin.watches', {
+                url: 'watches?key&value',
+                templateUrl: '../views/admin/admin.watches.html',
+                controller: 'WatchCtrl as vm',
+                data: {
+                    is_granted: ['ROLE_ADMIN']
+                }
+            });
+    }
+
+})();
