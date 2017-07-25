ec7ed40cfcf1711ac92ae5b581166aa13b9cf978
James Le <le_j6@denison.edu>
2017-07-25 15:02:12 -0700

    Create Cart Module

---
+++ app/cart/cart.mdl.js
@@ -0,0 +1,19 @@
+(function () {
+'use strict';
+
+angular
+    .module('cart', [
+        'cart.checkout'
+    ])
+    .config(config);
+
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
+function config($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('main.cart', {
+            url: 'cart',
+            templateUrl: '../views/cart/cart.html'
+        });
+}
+})();
