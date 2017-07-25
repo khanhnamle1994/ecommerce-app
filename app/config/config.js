e145578f56c638fcd18664621814664f3bd5471c
James Le <le_j6@denison.edu>
2017-07-25 15:10:14 -0700

    Config File

---
+++ app/config/config.js
@@ -0,0 +1,7 @@
+angular.module("config", [])
+.constant("BUCKET_SLUG", "ecommerce-app")
+.constant("MEDIA_URL", "https://api.cosmicjs.com/v1/ecommerce-app/media")
+.constant("URL", "https://api.cosmicjs.com/v1/")
+.constant("READ_KEY", "")
+.constant("WRITE_KEY", "")
+.constant("STRIPE_KEY", "pk_test_oRv6WcnATRyMqponKKG6QlON");
