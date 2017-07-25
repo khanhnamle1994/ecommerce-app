54e3f79ff225edb9d09a60a4e9906ad3235fe8fc
James Le <le_j6@denison.edu>
2017-07-25 15:06:05 -0700

    Create Admin Orders Module

---
+++ app/admin/orders/admin.orders.mdl.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('admin.orders', [
+            'admin.orders.preview'
+        ])
+        .config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('admin.orders', {
+                url: 'orders?key&value',
+                templateUrl: '../views/admin/admin.orders.html',
+                controller: 'AdminOrdersCtrl as vm',
+                data: {
+                    is_granted: ['ROLE_ADMIN']
+                }
+            });
+
+
+    }
+
+})();
