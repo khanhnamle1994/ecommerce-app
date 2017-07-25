17df668dd81567f0e286dbd4d7f736419c76f4cb
James Le <le_j6@denison.edu>
2017-07-25 15:03:02 -0700

    Create Cart Checkout Module

---
+++ app/cart/checkout/cart.checkout.mdl.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('cart.checkout', [])
+        .config(config);
+
+    config.$inject = ['$stateProvider', 'StripeCheckoutProvider'];
+    function config($stateProvider, StripeCheckoutProvider) {
+
+        $stateProvider
+            .state('main.cart.checkout', {
+                url: '/checkout',
+                views: {
+                    '@main': {
+                        templateUrl: '../views/cart/cart.checkout.html'
+                    }
+                }
+            })
+            .state('main.cart.thankYou', {
+                url: '/thank-you',
+                views: {
+                    '@main': {
+                        templateUrl: '../views/cart/cart.thank-you.html'
+                    }
+                }
+            });
+    }
+})();
