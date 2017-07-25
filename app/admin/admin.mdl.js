fbc87196e2a43552555668c754fcebc4fd38f144
James Le <le_j6@denison.edu>
2017-07-25 15:04:12 -0700

    Create Admin Module

---
+++ app/admin/admin.mdl.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('admin', [
+            'admin.watches',
+            'admin.orders'
+        ])
+        .config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('admin', {
+                url: '/admin/',
+                abstract: true,
+                templateUrl: '../views/admin/admin.html',
+                // controller: 'AdminCtrl as admin',
+                data: {
+                    is_granted: ['ROLE_ADMIN']
+                }
+            });
+    }
+
+})();
