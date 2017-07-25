91fff0661a39ce0a352d8f6b18b8173a9c9c67db
James Le <le_j6@denison.edu>
2017-07-25 14:54:55 -0700

    Create Main Module

---
+++ app/main.mdl.js
@@ -0,0 +1,112 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('main', [
+            'ui.router',
+            'ui.bootstrap',
+            'ngMask',
+            'ngCookies',
+            'ngRoute',
+            'ngDialog',
+            'cr.acl',
+            'ui-notification',
+            'ngFlash',
+            'textAngular',
+            'flow',
+            'angular-loading-bar',
+            'hl.sticky',
+
+            'watch',
+            'cart',
+            'admin',
+
+            'config'
+        ])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider', 'cfpLoadingBarProvider', 'NotificationProvider'];
+    function config($stateProvider, $urlRouterProvider, cfpLoadingBarProvider, NotificationProvider) {
+        cfpLoadingBarProvider.includeSpinner = false;
+
+        NotificationProvider.setOptions({
+            startTop: 25,
+            startRight: 25,
+            verticalSpacing: 20,
+            horizontalSpacing: 20,
+            positionX: 'right',
+            positionY: 'bottom'
+        });
+
+        $urlRouterProvider.otherwise(function ($injector) {
+            var $state = $injector.get("$state");
+            var $location = $injector.get("$location");
+            var crAcl = $injector.get("crAcl");
+
+            var state = "";
+
+            switch (crAcl.getRole()) {
+                case 'ROLE_ADMIN':
+                    state = 'admin.watches';
+                    break;
+                default : state = 'main.watch';
+            }
+
+            if (state) $state.go(state);
+            else $location.path('/');
+        });
+
+        $stateProvider
+            .state('main', {
+                url: '/',
+                abstract: true,
+                templateUrl: '../views/main.html',
+                controller: 'CartCtrl as cart',
+                data: {
+                    is_granted: ['ROLE_GUEST']
+                }
+            })
+            .state('blog', {
+                url: '/blog',
+                templateUrl: '../blog.html'
+            })
+            .state('auth', {
+                url: '/login',
+                templateUrl: '../views/auth/login.html',
+                controller: 'AuthCtrl as auth',
+                onEnter: ['AuthService', 'crAcl', function(AuthService, crAcl) {
+                    AuthService.clearCredentials();
+                    crAcl.setRole();
+                }],
+                data: {
+                    is_granted: ['ROLE_GUEST']
+                }
+            });
+    }
+
+    run.$inject = ['$rootScope', '$cookieStore', '$state', 'crAcl'];
+    function run($rootScope, $cookieStore, $state, crAcl) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+
+        crAcl
+            .setInheritanceRoles({
+                'ROLE_ADMIN': ['ROLE_ADMIN', 'ROLE_GUEST'],
+                'ROLE_GUEST': ['ROLE_GUEST']
+            });
+
+        crAcl
+            .setRedirect('main.watch');
+
+        if ($rootScope.globals.currentUser) {
+            crAcl.setRole($rootScope.globals.currentUser.metadata.role);
+            // $state.go('admin.watches');
+        }
+        else {
+            crAcl.setRole();
+        }
+
+    }
+
+})();
